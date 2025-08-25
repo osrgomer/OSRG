8e710dd76c6af4516d561ac8e3a782950edd8dbb
OSRG <your.email@example.com>
2025-08-25 14:24:46 +0100

    changed timezone to Europe/London

--- private_social_platform/config.php
+++ private_social_platform/config.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-date_default_timezone_set('Europe/Amsterdam'); // Adjust to your timezone
+date_default_timezone_set('Europe/London'); // Adjust to your timezone
 
 // Database configuration
 $db_file = 'private_social.db';

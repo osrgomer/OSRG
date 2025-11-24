9192f05f0d301a41ad55d2c4e511c508d3aca7aa
OSRG <omersr12@gmail.com>
2025-11-24 17:31:57 +0000

    Fix tea-page.php to read from CSV and display tea details properly

--- apps/tea/tea-page.php
+++ apps/tea/tea-page.php
@@ -6,12 +6,28 @@
 </head>
 <body>
 <?php
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 
-require ("functions.php");
-$id = $_GET['id'] + 1;
+$csvFile = fopen('tea.csv', 'r');
+if (!$csvFile) {
+    die('Error: Cannot open tea.csv file.');
+}
 
-    get_tea($id);
+$counter = 0;
+$title = $description = $buy = $wikilink = $image_url = '';
 
+while (($line = fgetcsv($csvFile)) !== false) {
+    if ($counter == $id) {
+        $title = isset($line[0]) ? $line[0] : '';
+        $description = isset($line[1]) ? $line[1] : '';
+        $image_url = isset($line[29]) ? $line[29] : '';
+        $wikilink = isset($line[30]) ? $line[30] : '';
+        $buy = isset($line[31]) ? $line[31] : '';
+        break;
+    }
+    $counter++;
+}
+fclose($csvFile);
 ?>
 
 <h2><?php echo $title; ?></h2>
@@ -19,22 +35,27 @@ $id = $_GET['id'] + 1;
 
 <h2>Description: <?php echo $description; ?></h2>
 <br/>
+<?php if($image_url): ?>
+<img src="<?php echo htmlspecialchars($image_url); ?>" alt="<?php echo htmlspecialchars($title); ?>" style="max-width: 300px; height: auto;">
+<br/><br/>
+<?php endif; ?>
+
+<?php if($buy): ?>
 <h2>to buy:</h2>
  <div class="itemdiv">
-	  <a href="https://www.naturitas.pt/p/alimentacao/bebidas/chas-e-infusoes/infusoes-relaxantes/cha-de-alcacuz-17-saquetas-de-infusao-yogi-tea">
-	    <h2 class="itemheading"><?php echo $buy; ?></h2>
+	  <a href="<?php echo htmlspecialchars($buy); ?>">
+	    <h2 class="itemheading">Buy <?php echo htmlspecialchars($title); ?></h2>
  	  </a>
  	</div>
+<?php endif; ?>
 
-
-
-
-
+<?php if($wikilink): ?>
 <div class="itemdiv">
-	  <a href="https://en.wikipedia.org/wiki/Liquorice">
-	    <h2 class="itemheading"><?php echo $wikilink; ?></h2>
+	  <a href="<?php echo htmlspecialchars($wikilink); ?>">
+	    <h2 class="itemheading">Learn more about <?php echo htmlspecialchars($title); ?></h2>
  	  </a>
  	</div>
+<?php endif; ?>
 
 
 
@@ -50,6 +71,6 @@ $id = $_GET['id'] + 1;
 
 
 
-<h1><a href="/">Back to website</a></h1>
+<h1><a href="tea.php">Back to Tea Time</a></h1>
 
 </body>
